3846c3a9845f3cc8b36efba92f0e9fb5d45fd0da
Medger Deana <medeana@at-n5.msk.21-school.ru>
2022-07-06 17:08:39 +0300

    Element access, Capacity

--- main.cpp
+++ main.cpp
@@ -4,4 +4,7 @@
 int main(){
     std::vector<int> v1(10);
     ft::vector<int> v2(10);
+
+    std::cout << v1.at(10) << std::endl;
+    // std::cout << v2.get_allocator() << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -23,6 +23,8 @@ namespace ft{
         typedef typename std::ptrdiff_t                     difference_type;
 
 
+        //Member functions
+
         explicit vector(const allocator_type& alloc = allocator_type()) : arr(0), sz(0), cap(0), allocator(alloc) {
         };
 
@@ -32,6 +34,10 @@ namespace ft{
                     for (size_t i = 0; i < n; i++)
                         allocator.construct(arr + i, val);
                  };
+
+                // template <class InputIterator>
+                // vector (InputIterator first, InputIterator last,  const allocator_type& alloc = allocator_type());
+
 
         vector (const vector<value_type> &x) : cap(x.cap), sz(x.sz), allocator(x.allocator) {
             arr = allocator.allocate(x.cap);
@@ -57,17 +63,98 @@ namespace ft{
             return (*this);
         };
 
+        // Iterators:
+                // template <class InputIterator>
+                // void assign (InputIterator first, InputIterator last);
+
+        // Capacity:
+
+        // void resize (size_type n, value_type val = value_type()){}
+
+        // void reserve (size_type n);
+
+        size_type size() const{
+            return this->sz;
+        }
+
+        size_type max_size() const{
+            return this->allocator.max_size();
+        }
+
+        size_type capacity() const{
+            return this->cap;
+        }
+
+        bool empty() const{
+            return sz > 0 ? 0 : 1;
+        }
+
+        // Element access:
+
+        reference   operator[](size_type n){
+            return (arr[n]);
+        }
+
+        const_reference operator[](size_type n) const{
+            return (arr[n]);
+        }
+
+        reference at(size_type n)
+        {
+            if (n >= sz)   
+                throw std::out_of_range("vector");
+            else
+                return (arr[n]);
+        }
+        const_reference at(size_type n) const{
+            if (n >= sz)   
+                throw std::out_of_range("vector");
+            else
+                return (arr[n]);
+        }
+
+        reference front(){
+            return arr[0];
+        }
+
+
+        const_reference front() const{
+            return arr[0];
+        }
+
+        reference back(){
+            return arr[sz - 1];
+        }
+
+        const_reference back() const{
+            return arr[sz - 1];
+        }
+        
+        // Modifiers:
+
         void assign (size_type n, const value_type& val){
             clear();
             if (n > cap){
                 if (arr)
                     allocator.deallocate(arr, cap);
                 cap = n;
-                arr = allocator.allocate(x.cap);
+                arr = allocator.allocate(cap);
             }
-            for (size = 0; size < n; size++)
-                allocator.construct(arr + size, val);
+            for (sz = 0; sz < n; sz++)
+                allocator.construct(arr + sz, val);
         }
+
+        void     clear()
+        {
+            for (size_type i = 0; i < sz; i++)
+                allocator.destroy(arr + i);
+            sz = 0;
+        }
+
+        allocator_type get_allocator() const{
+            return this->allocator;
+        }
+
 
     private:
         pointer arr;
