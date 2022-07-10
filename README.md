3b57aa72559ef1e74d9fb3e24439ee3a838ada5f
Medger Deana <medeana@at-d4.msk.21-school.ru>
2022-07-10 18:16:46 +0300

    movement

--- README.md
+++
@@ -1,1 +0,0 @@
-# STL_Containers
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -4,7 +4,6 @@
 int main(){
     std::vector<int> v1(5, 1);
     ft::vector<int> v2(5, 6);
-    ft::vector<int> v3(5, 4);
 
     // std::cout << v1.size() << std::endl;
     // std::cout << v2.size() << std::endl;
@@ -28,13 +27,12 @@ int main(){
     // std::cout << " " << std::endl;
     // std::cout << " " << std::endl;
 
-    // v1.pop_back();
-    // v2.pop_back();
+    v1.assign(2, 4);
+    v2.assign(2, 4);
 
     // for (int i = 0; i < 6; i++){
     //     std::cout << "V1 = " << v1[i] << "  " << "V2 = " << v2[i] << std::endl;
     // }
-    v2.swap(v3);
 
     std::cout << " " << std::endl;
     std::cout << " " << std::endl;
@@ -49,9 +47,9 @@ int main(){
     // std::cout << " " << std::endl;
     // std::cout << " " << std::endl;
 
-    // std::cout << "size " << v1.size() << std::endl;
-    // std::cout << "size " << v2.size() << std::endl;
+    std::cout << "size " << v1.size() << std::endl;
+    std::cout << "size " << v2.size() << std::endl;
 
-    // std::cout << v1.capacity() << std::endl;
-    // std::cout << v2.capacity() << std::endl;
+    std::cout << v1.capacity() << std::endl;
+    std::cout << v2.capacity() << std::endl;
 }
